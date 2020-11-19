7259612e59dd35f28ce007e762a778c0a8f5bf0a
JAlbertoLeal <juan.selfaleal@gmail.com>
2020-11-19 22:41:50 +0100

    Preparando el Pull

--- src/carolinaadivino/CarolinaAdivino.java
+++ src/carolinaadivino/CarolinaAdivino.java
@@ -17,7 +17,7 @@ public class CarolinaAdivino {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int n = 0,cont = 0;		//Perfecto cumple el objetivo. Revisado por Juan Alberto.
+        int n = 0,cont = 0;
         int x = (int) (Math.random()*(10-50)+50);
         //System.out.print(x);
         Scanner sc = new Scanner(System.in);
